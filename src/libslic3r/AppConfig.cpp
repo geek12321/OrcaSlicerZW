a8ab717df666bd5e752388fbf09d83e398821e7e
geek12321 <77199083+geek12321@users.noreply.github.com>
2026-05-31 15:43:10 +0800

    Update AppConfig.cpp
    jinyonggengxindizhi

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -39,8 +39,8 @@ using namespace nlohmann;
 
 namespace Slic3r {
 
-static const std::string VERSION_CHECK_URL = "https://check-version.orcaslicer.com/latest";
-static const std::string PROFILE_UPDATE_URL = "https://api.github.com/repos/OrcaSlicer/orcaslicer-profiles/releases/tags";
+static const std::string VERSION_CHECK_URL = "https://127.0.0.1";
+static const std::string PROFILE_UPDATE_URL = "https://127.0.0.1";
 static const std::string MODELS_STR = "models";
 
 const std::string AppConfig::SECTION_FILAMENTS = "filaments";
